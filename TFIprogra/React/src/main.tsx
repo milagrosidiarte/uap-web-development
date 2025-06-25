2e47b6118f52c650141375ddc4aa9b25e6d8077b
Milagros Idiarte <milagros.idiarte@uap.edu.ar>
2025-06-25 16:11:37 -0300

    guardando cambios

--- TFIprogra/React/src/main.tsx
+++ TFIprogra/React/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.ccs'
+import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
