6bb36da957022a5d6f896031b828501f4f6b229c
milagrosidiarte <milagros.idiarte@uap.edu.ar>
2025-10-01 03:15:11 -0300

    mas config de tailwind

--- faucet-dapp/tailwind.config.js
+++ faucet-dapp/tailwind.config.js
@@ -1,10 +1,11 @@
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 importante
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
